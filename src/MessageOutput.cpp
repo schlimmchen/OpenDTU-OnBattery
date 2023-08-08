a87f7115b61062bb24f02da02a31872afe3a8be4
Bernhard Kirchen <schlimmchen@posteo.net>
2023-08-08 21:47:05 +0200

    MessageOutput: respect HardwareSerial buffer size
    the MessageOutput class buffers whole lines of output printed by any
    task in order to avoid mangling of text. that means we hand over full
    lines to the HardwareSerial instance, which might be too much in one
    call to write(buffer, size). we now check the return value of
    write(buffer, size) and call the function again with the part of the
    message that could not yet be written by HardwareSerial.

--- src/MessageOutput.cpp
+++ src/MessageOutput.cpp
@@ -14,6 +14,21 @@ void MessageOutputClass::register_ws_output(AsyncWebSocket* output)
     _ws = output;
 }
 
+void MessageOutputClass::serialWrite(MessageOutputClass::message_t const& m)
+{
+    // on ESP32-S3, Serial.flush() blocks until a serial console is attached.
+    // operator bool() of HWCDC returns false if the device is not attached to
+    // a USB host. in general it makes sense to skip writing entirely if the
+    // default serial port is not ready.
+    if (!Serial) { return; }
+
+    size_t written = 0;
+    while (written < m.size()) {
+        written += Serial.write(m.data() + written, m.size() - written);
+    }
+    Serial.flush();
+}
+
 size_t MessageOutputClass::write(uint8_t c)
 {
     std::lock_guard<std::mutex> lock(_msgLock);
@@ -25,7 +40,7 @@ size_t MessageOutputClass::write(uint8_t c)
     message.push_back(c);
 
     if (c == '\n') {
-        Serial.write(message.data(), message.size());
+        serialWrite(message);
         _lines.emplace(std::move(message));
         _task_messages.erase(iter);
     }
@@ -49,7 +64,7 @@ size_t MessageOutputClass::write(const uint8_t *buffer, size_t size)
         message.push_back(c);
 
         if (c == '\n') {
-            Serial.write(message.data(), message.size());
+            serialWrite(message);
             _lines.emplace(std::move(message));
             message.clear();
             message.reserve(size - idx - 1);
